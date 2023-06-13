684eb3fbb57ffb90f53133a9e33e3c800d2ff4f9
Diele <dybs1520@gmail.com>
2023-06-13 16:13:10 -0300

    CRIIEI CLASSE PET

---
+++ VariasClasses/VariasClasses/Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace VariasClasses.Models
+{
+    public class Pet
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Raca { get; set; }
+        public string Patologia { get; set; }
+    }
+}
