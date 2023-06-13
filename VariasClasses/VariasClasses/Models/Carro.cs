6508d1269b8bc951d4b9fbfe424011f6ba2eb526
Diele <dybs1520@gmail.com>
2023-06-13 15:50:52 -0300

    CRIEI A CLASSE CARRO

---
+++ VariasClasses/VariasClasses/Models/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exerciciovariasclasses.Models
+{
+    public class Carro
+    {
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Fabricante { get; set; }
+    }
+}
