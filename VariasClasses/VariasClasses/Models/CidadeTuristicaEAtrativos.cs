fab36825b211e67050146d36461a124cefafe3c0
Diele <dybs1520@gmail.com>
2023-06-13 16:11:32 -0300

    ATUALIZAÇÃO- INCLUINDO INTENS NA CLASSE

--- VariasClasses/VariasClasses/Models/CidadeTuristicaEAtrativos.cs
+++ VariasClasses/VariasClasses/Models/CidadeTuristicaEAtrativos.cs
@@ -1,6 +1,15 @@
-﻿namespace VariasClasses.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VariasClasses.Models
 {
     public class CidadeTuristicaEAtrativos
     {
+        public string NomeCidade { get; set; }
+        public string Uf { get; set; }
+        public string Pais { get; set; }
+        public string Atrativo { get; set; }
     }
 }
